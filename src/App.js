101b54841761184833deab86bd104c949b5d122b
ttoy12 <thomastoy1415@gmail.com>
2024-05-19 22:15:19 -0700

    Added routing and made 3 pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, Container } from '@mui/material';
+import Selection from './pages/Selection';
+import Spinner from './pages/Spinner';
+import Destination from './pages/Destination';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Restaurant Roulette
+          </Typography>
+          <Button color="inherit" component={Link} to="/">Selection</Button>
+          <Button color="inherit" component={Link} to="/Spinner">Spinner</Button>
+          <Button color="inherit" component={Link} to="/Destination">Destination</Button>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Selection />} />
+          <Route path="/Spinner" element={<Spinner />} />
+          <Route path="/Destination" element={<Destination />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
